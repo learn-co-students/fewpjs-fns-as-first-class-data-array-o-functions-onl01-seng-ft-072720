5f423432a0fca2477063747f49882061255eba8a
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-25 00:34:22 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,28 @@ function leashDog(dogName, dogBreed) {
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk ${dogName}`)
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+
+function walkHome(dogName, dogBreed){
+  console.log(`Walk home with ${dogName} the ${dogBreed}`)
+  return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dogName, dogBreed) {
+  let l = routine.map(fn => fn(dogName,dogBreed))
+  return l
 }
